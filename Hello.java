e9d7364f05d58a56d809755ec906dd81e80348d0
mldn <mldnqa@163.com>
2018-02-27 20:55:34 +1100

    Test Merge

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@ public class Hello{
 		System.out.println("世界，你好！");
 		System.out.println("世界，你好！");
 		System.out.println("世界，你好！");
+		System.out.println("世界，你好！");
+		System.out.println("世界，你好！");
 		Admin admin = new Admin();
 		Emp emp = new Emp();
 		System.out.println("临近中午适合学习，因为饿了");
